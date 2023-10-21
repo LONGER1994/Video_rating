693b08182231f99226f5ebbd8ef08cdb2aa267a3
Ethan Long <ethanlong41152@gmail.com>
2023-10-21 19:07:57 +0800

    adding new pages

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("front_page.html")
+    return render_template("home_page.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
