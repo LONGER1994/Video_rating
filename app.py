94f24f14734c4b31fc46036827da6bdadaf3f926
Ethan Long <ethanlong41152@gmail.com>
2023-12-03 15:51:43 +0800

    adding details to the pages and connect them in app.py

--- app.py
+++ app.py
@@ -8,5 +8,17 @@ def index():
     return render_template("home_page.html")
 
 
+# 按下search之後顯示的頁面
+@app.route("/result")
+def search():
+    return render_template("search_result.html")
+
+
+# 用movie_id
+@app.route("/movie")
+def movie():
+    return render_template("movie_page.html")
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001, debug=True)
